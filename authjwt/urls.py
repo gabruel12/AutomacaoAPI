7e91bfc5cf12d829e5dc9fc677985afc1e2e07d8
Luiz Eduardo Rodrigues <naoeoserafim@gmail.com>
2026-02-22 21:56:20 -0300

    Correção authjwt urls

--- authjwt/urls.py
+++ authjwt/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
-from .views import CadasterView, LoginView, DeleteView, EditView, VerifyEmailSerializer
+from .views import CadasterView, LoginView, DeleteView, EditView, VerifyEmailView
 
 urlpatterns = [
     path("cadaster/", CadasterView.as_view(), name="cadaster"),
     path("login/", LoginView.as_view(), name="login"),
     path("delete/", DeleteView.as_view(), name="delete"),
     path("edit/", EditView.as_view(), name="edit"),
-    path("verify/", VerifyEmailSerializer.as_view(), name="verify")
+    path("verify/", VerifyEmailView.as_view(), name="verify")
 ]
